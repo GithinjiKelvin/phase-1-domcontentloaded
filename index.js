741f2534052e85757ccec1da7c985e33db6b17c3
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-07-31 02:34:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", changePText)
+
+function changePText(){
+    let par = document.getElementById("text")
+    par.textContent = "This is really cool!"
+}
